2a34c8f24a8363bf5e5c9cbfefe24fe2ca9a5f1c
Souvik9735 <65434733+Souvik9735@users.noreply.github.com>
2021-10-04 18:22:15 +0530

    Update spambot.py

--- spambot.py
+++ spambot.py
@@ -2,5 +2,5 @@ import pyautogui
 import time
 time.sleep(5)
 for x in range(100):
-    pyautogui.typewrite("I am sorry...tmr toh ar dorkar nei akhon amake jani....ar hoyto valobasatao nei...ager moton hoe jao pls r valo lagchenah......nah koro shei bachhami...thik mton kothao boloonah...joto ta sombhob dure thaka jay shei chesta..khali kharap gulo dekhe gele....kono valo i ki nei???ato ghora adda mara moja kora amr room ...onek valo din o geche go.ager moton kotha boli abar amra??jerkm vid cl sob kichu hoto serkm??..kothata onek din pore bolchi thik i...r bolar mukh o nei akhn....tao bolchi...i love you")
+    pyautogui.typewrite("message")
     pyautogui.press("enter")
